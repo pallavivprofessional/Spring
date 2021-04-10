63e1f1bb0a19966df91fc35db17643262c88045d
pallavivprofessional <pallavivprofessional@gmail.com>
2021-04-10 18:34:28 +0530

    After revert

---
+++ src/main/java/SpringGroupId/Spring_Project/App.java
@@ -0,0 +1,14 @@
+package SpringGroupId.Spring_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+   // 	ApplicationCo
+        System.out.println( "Hello World!" );
+    }
+}
